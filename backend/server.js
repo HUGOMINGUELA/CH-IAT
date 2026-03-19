24ca74afd18e9f884c1d445812841ef8bce73ef6
Hugo  Aguilar <mantenimiento@tiser.onmicrosoft.com>
2026-03-19 17:34:00 -0600

    fix syntax error

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
 });
-
-app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto " + PORT);
-});
-  console.log("Servidor listo en puerto 3001");
-});

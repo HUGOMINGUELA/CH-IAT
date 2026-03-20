93e1689a2b2313637df5bb40a205309ba880051b
Hugo  Aguilar <mantenimiento@tiser.onmicrosoft.com>
2026-03-19 18:16:19 -0600

    fix cors y json

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Sistema Capital Humano funcionando 🚀");
 });
+
 app.post("/finiquito", (req, res) => {
 
   const { salario, diasTrabajados, diasVacaciones, diasAguinaldo } = req.body;
 
   const salarioDiario = salario / 30;
 
-  // Aguinaldo proporcional
   const aguinaldo = (salarioDiario * diasAguinaldo / 365) * diasTrabajados;
-
-  // Vacaciones proporcionales
   const vacaciones = (salarioDiario * diasVacaciones / 365) * diasTrabajados;
-
-  // Prima vacacional 25%
   const primaVacacional = vacaciones * 0.25;
 
-  // Total
   const total = salarioDiario * diasTrabajados + aguinaldo + vacaciones + primaVacacional;
 
   res.json({
@@ -31,6 +31,7 @@ app.post("/finiquito", (req, res) => {
     total
   });
 });
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
